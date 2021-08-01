3b0e4bf094ffa53d4a7fe1674ae1048f79850e2b
Tijil <junejatijil4@gmail.com>
2021-08-01 23:11:51 +0530

    CRUD Functionalities added

--- src/main/java/co/movie/movie/MovieController.java
+++ src/main/java/co/movie/movie/MovieController.java
@@ -1,10 +1,15 @@
 package co.movie.movie;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,7 +24,19 @@ public class MovieController {
         return movieService.findAll();
     }
     @GetMapping(value = "/movies/{id}")
-    public Movie getMovie(){
-        return movieService.findById();
+    public Optional<Movie> getMovie(@PathVariable String id){
+        return movieService.findById(id);
+    }
+    @PostMapping(value = "/movies")
+    public void insertMovie(@RequestBody Movie movie){
+        movieService.insertMovie(movie);
+    }
+    @PostMapping(value = "/movies")
+    public void updateMovie(@RequestBody Movie movie){
+        movieService.updateMovie(movie);
+    }
+    @DeleteMapping(value = "/movies/{id}")
+    public void deleteMovie(@PathVariable String id){
+        movieService.deleteMovie(id);
     }
 }
--- src/main/java/co/movie/movie/MovieService.java
+++ src/main/java/co/movie/movie/MovieService.java
@@ -1,11 +1,30 @@
 package co.movie.movie;
 
 import java.util.List;
+import java.util.Optional;
+
 
 public class MovieService {
 
+    MovieRepository movieRepository;
     public List<Movie> findAll() {
-        return null;
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(String id) {
+        return movieRepository.findById(id);
+    }
+
+    public void insertMovie(Movie movie) {
+        movieRepository.insert(movie);
+    }
+
+    public void updateMovie(Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    public void deleteMovie(String id) {
+        movieRepository.deleteById(id);
     }
     
 }
